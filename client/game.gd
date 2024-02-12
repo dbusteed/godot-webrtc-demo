12f0cc7bc0b263b0b0bef7927e49063cc9611d33
dbusteed <busteed.davis@gmail.com>
2024-02-12 15:32:26 -0500

    added build

--- client/game.gd
+++ client/game.gd
@@ -5,5 +5,3 @@ func setup_camera():
 	var cam = $Camera2D
 	cam.get_parent().remove_child(cam)
 	get_node("Players/" + str(multiplayer.get_unique_id())).add_child(cam)
-
-# web -> HTTPS -> WSS -> SSL Cert -> DNS 
